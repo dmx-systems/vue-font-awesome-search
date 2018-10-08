838d25a055e2f3c51722a7d6b98dd26c466a25f6
Jörg Richter <jri@deepamehta.de>
2018-10-08 16:04:19 +0200

    Initial commit

---
+++ src/components/font-awesome-search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span v-for="(icon, i) in icons" :class="['fa', `fa-${icon.id}`]"></span>
+  </div>
+</template>
+
+<script>
+import icons from '../icons'
+
+export default {
+
+  created () {
+    console.log('font-awesome-search created', this.icons)
+  },
+
+  data () {
+    return {
+      icons
+    }
+  }
+}
+</script>
+
+<style>
+</style>
