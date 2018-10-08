a20a026d78e0d8c9af6c19dce723c075d7836f14
Jörg Richter <jri@deepamehta.de>
2018-10-08 18:20:33 +0200

    Emit `icon-select` event

--- src/components/font-awesome-search.vue
+++ src/components/font-awesome-search.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <span v-for="(icon, i) in icons" :class="['fa', `fa-${icon.id}`]"></span>
+  <div class="font-awesome-search">
+    <el-button v-for="icon in icons" :key="icon.id" @click="click(icon)">
+      <span :class="['icon', 'fa', `fa-${icon.id}`]"></span>
+      <span>{{icon.name}}</span>
+    </el-button>
   </div>
 </template>
 
@@ -17,9 +20,18 @@ export default {
     return {
       icons
     }
+  },
+
+  methods: {
+    click (icon) {
+      this.$emit('icon-select', icon)
+    }
   }
 }
 </script>
 
 <style>
+.font-awesome-search .icon{
+  margin-right: 4px;
+}
 </style>
